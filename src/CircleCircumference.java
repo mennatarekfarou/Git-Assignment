2405400402223859579146919edda9c1de6156d1
Mariam Ahmad <36169438+mariam-beshary9@users.noreply.github.com>
2020-02-28 20:59:09 +0200

    Update CircleCircumference.java

--- src/CircleCircumference.java
+++ src/CircleCircumference.java
@@ -3,8 +3,8 @@ public class CircleCircumference implements ISubscriber {
 
 	@Override
 	public void notifySubscriber(String input) {
-		int diameter = Integer.parseInt(input);
-		System.out.println("The circumference of the circle = " + 2*diameter*Math.PI);
+		int radius = Integer.parseInt(input);
+		System.out.println("The circumference of the circle = " + 2*radius*Math.PI);
 	}
 
 }
