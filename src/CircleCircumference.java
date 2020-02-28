02c779e118f7fd3672a1562a8e7c9d7c8094a114
Mariam Ahmad <36169438+mariam-beshary9@users.noreply.github.com>
2020-02-28 12:59:58 +0200

    Creating CircleCircumference class
    my ID :  20170279

---
+++ src/CircleCircumference.java
@@ -0,0 +1,10 @@
+
+public class CircleCircumference implements ISubscriber {
+
+	@Override
+	public void notifySubscriber(String input) {
+		int diameter = Integer.parseInt(input);
+		System.out.println("The circumference of the circle = " + 2*diameter*Math.PI);
+	}
+
+}
