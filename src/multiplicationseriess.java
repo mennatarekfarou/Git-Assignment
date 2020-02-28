bf1fc2e63cc483f4fb8d608a438008eacd2c6213
mennatarekfarou <44003839+mennatarekfarou@users.noreply.github.com>
2020-02-28 21:33:28 +0200

    Update multiplicationseriess.java

--- src/multiplicationseriess.java
+++ src/multiplicationseriess.java
@@ -1,18 +1,19 @@
+
 public class Multiplicationseriess implements ISubscriber {
     public int m(int n){
-     int result =1;
-     for(int i=1; i<=n ; i++){
-         result=result*i;
+        int result =1;
+        for(int i=1; i<=n ; i++){
+            result=result*i;
+        }
+       return result ;
+       }
+      @Override
+   	public void notifySubscriber(String input) {
+         System.out.print("Hello,I am a multiplication series and  I am notified with"+input);
+         System.out.print( "and my result is : " + m(Integer.parseInt(input)));
+         
+         
+         
      }
-    return result ;
-    }
-   @Override
-	public void notifySubscriber(String input) {
-      System.out.print("Hello,I am a multiplication series and  I am notified with"+input);
-      System.out.print( "and my result is : " + m(Integer.parseInt(input)));
-      
-      
-      
-  }
-    
-}
+       
+   }
